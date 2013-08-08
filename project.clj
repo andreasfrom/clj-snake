073fe2f5f40f26808e1aba6d0b2961296a039bb7
Andreas From <andro.from@gmail.com>
2013-08-08 16:18:34 +0200

    update description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-snake "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple Snake game"
+  :url "https://github.com/andreasfrom/clj-snake"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
